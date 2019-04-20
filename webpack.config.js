79d02fbed76b042c993739fd6d0199ef308f591b
Marketa <markettka@email.cz>
2019-04-20 19:52:20 +0200

    set up webpack,babel, html template

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+//import path from 'path'
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'], 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js' 
+  },
+  //mode: 'development',
+  devServer: {
+    contentBase: './dist'  // webpack serving file
+   },  // automatic reload and bundle
+   plugins: [
+     new HtmlWebpackPlugin({
+       filename: 'index.html',
+       template: './src/index.html'
+     }) // copy index.html to the dist index.html
+   ],
+   module: {
+     rules: [
+       {
+         test: /\.js$/, // look for all js files
+         exclude: /node_modules/,
+         use: {
+           loader: 'babel-loader'
+         }
+        }
+     ]
+   }
+}
